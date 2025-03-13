36779685bb82d65c1b72fc8c960bec9bf820c7ac
aziz <azizswe.ksu@gmail.com>
2025-03-14 01:47:26 +0300

    Make migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        """Create and return a new superuser"""
+        user = self.create_user(email, name, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="userprofile_set",  # Prevents clash with default User model
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="userprofile_permissions_set",  # Prevents clash with default User model
+        blank=True,
+    )
+
+    objects = UserProfileManager()  # Now correctly defined
+
+    USERNAME_FIELD = 'email'  # Required by default
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name  # Fixed missing return statement
+
+    def __str__(self):
+        """Return string representation of our user"""
+        return self.email
