6b7f91b16dcdd3d5f5a1911815958af5d4590ad0
aziz <azizswe.ksu@gmail.com>
2025-03-15 03:08:39 +0300

    h

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test"""
+
+    def get(self, request, Format=None):
+        """Retrun a list of APIView features"""
+        an_apiview = [
+            "F1",
+            "F2",
+            "F3"
+        ]
+
+        return Response({"Message":"welcome","an_apiview":an_apiview}) 
+
